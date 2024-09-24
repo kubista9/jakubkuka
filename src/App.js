3ac035c036ba8a1ff0a8ead6c8efda276da09d82
Jakub Kuka <jakubkuka07@gmail.com>
2024-09-24 22:16:36 +0200

    Project structure added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Jakub Kuka </h1>
+      <h1>Jakub Kuka</h1>
+      <h2>About me</h2>
+      <h2>Stand up comedy</h2>
+      <h2>Software engineering</h2>
     </div>
   );
 }
